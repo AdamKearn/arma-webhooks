94b25ba3b32ef0e5b4966d542520e5f874f616ab
Adam Kearn <adamkearn17@gmail.com>
2023-11-12 01:40:15 +0000

    adding basic way of calling a discord webhook

--- src/discord.rs
+++ src/discord.rs
@@ -1,9 +1,20 @@
+use crate::RUNTIME;
 use arma_rs::Group;
-
-pub fn send() -> &'static str {
-    "this is the responce back from discord...yayaya!"
-}
+use discord_webhook::client::WebhookClient;
 
 pub fn group() -> Group {
     Group::new().command("send", send)
+}
+
+fn send(message_body: String) -> String {
+    RUNTIME.block_on(async move {
+        let client: WebhookClient = WebhookClient::new("https://discord.com/api/webhooks/1168673937731891250/y5Wv3_pHoLHebrBP8lLVbonAX4z3nn_XyVbIKbxsEw3SUYyMjrZHXbM8ByN6_BF1xX6H");
+        let result = client
+            .send(|message| message.embed(|embed| embed.title("Webhook").description(&message_body)))
+            .await;
+
+        print!("{:?}", result);
+    });
+
+    format!("success")
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,14 @@
 use arma_rs::{arma, Extension};
+use once_cell::sync::Lazy;
 
 mod discord;
+
+static RUNTIME: Lazy<tokio::runtime::Runtime> = Lazy::new(|| {
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .expect("Unable to start the runtime.")
+});
 
 #[arma]
 fn init() -> Extension {
